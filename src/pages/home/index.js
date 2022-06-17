7cf3d59e19256f790bbc77efdaa989e0aff0b7be
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-06-16 21:15:39 -0400

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 import Pet from '../../components/pet';
-import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { useParams, Link } from "react-router-dom";
+
 
 const HomePage = () => {
   const [data, setData] = useState(null);
